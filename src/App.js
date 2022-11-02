4aef90a98389a43adb88c22ecb1b0224de9be38b
Grayce Muthui <graycemuthui20@gmail.com>
2022-11-03 01:24:06 +0300

    Create the leaderboard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
+import Leaderboard from "./components/Leaderboard/Leaderboard";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<Homepage />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
           </Routes>
         </Router>
       </main>
--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,7 +1,41 @@
+import { records } from "../data/Mockdata";
+import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
+
 const Leaderboard = () => {
+  const filters = records.sort((a, b) => {
+    if (a.edits > b.edits) {
+      return -1; //{1, 2, 3, 4, 4}
+    } else if (a.edits < b.edits) {
+      return 1;
+    } else return 0;
+  });
+
+  const nonZeroFilters = filters.filter((filter) => filter.edits > 0);
   return (
-    <div>
-      <h1>Leaderboard</h1>
-    </div>
+    <Container className="p-5">
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Edits</th>
+          </tr>
+        </thead>
+        <tbody>
+          {nonZeroFilters.map(({ id, name, edits }) => (
+            <tr key={id}>
+              <td>
+                <p>{name}</p>
+              </td>
+              <td>
+                <p>{edits}</p>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
   );
 };
+
+export default Leaderboard;
